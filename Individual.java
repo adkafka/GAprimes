d1119fb829239f3438d17c1715ef3b4626f17e7e
Adam Kafka <adkafka@gmail.com>
2014-08-01 12:51:00 -0400

    Made Individual implement comparable and fitness a double

--- Individual.java
+++ Individual.java
@@ -1,11 +1,11 @@
-public class Individual {
+public class Individual implements Comparable{
     //////////
     //Fields//
     //////////
 
     private ArithmeticTree equation; //Genome
 
-    private int fitness; //Numerical representation of the fitness
+    private double fitness; //Numerical representation of the fitness
     private static int[] key = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541};//List of 100 first primes
 
     ////////////////
@@ -27,21 +27,30 @@ public class Individual {
     //Methods//
     ///////////
     
+    /** Compare two individuals based on their fitness **/
+    public int compareTo(Object i2){
+        if(i2 instanceof Individual){
+            return Double.compare(fitness,((Individual)i2).fitness);
+        }else{
+            System.err.println("Incomparable types");
+            return Integer.MAX_VALUE;
+        }
+    }
+
     /** Calculates fitness of AT */
     public void calculateFitness(){
-        fitness = 0;//set fitness at 0 to begin
+        fitness = 0.0;//set fitness at 0 to begin
         for(int i=0; i<key.length; i++){//Calculate squared distance from each entry in key
             double val = equation.evaluate(i);
             double deltaSquared = Math.pow((val-key[i]),2);
-            fitness+=(int)deltaSquared;
-            System.out.println(i+": val="+val+" deltaSquared="+deltaSquared);
+            fitness+=deltaSquared;
+            //System.out.println(i+": val="+val+" deltaSquared="+deltaSquared);
             
         }
-
     }
 
     /** Getter for fitness */
-    public int getFitness(){
+    public double getFitness(){
         return fitness;
     }
 
@@ -53,17 +62,10 @@ public class Individual {
 
     /** Main method */
     public static void main(String[] args){
-        ArithmeticTree.setRandSeed(18);
-        ArithmeticTree t = new ArithmeticTree();
-        System.out.println(t.toString());
-        System.out.println(t.evaluate(2));
-        t.foldConstants();
-        System.out.println(t.toString());
-        for(Node n : t.nodes){
-            System.out.print(n.getSymbol()+", ");
-        }System.out.println();
-        System.out.println(t.evaluate(2));
-        System.out.println(t.nodes.size()+"\t"+t.count());
+        ArithmeticTree.setRandSeed(20);
+        //ArithmeticTree t = new ArithmeticTree();
+        //System.out.println(t.toString());
+        //System.out.println(t.evaluate(2));
         /*
         for(int i = 0; i<10000; i++){
             ArithmeticTree.setRandSeed(i);
@@ -80,7 +82,15 @@ public class Individual {
             }
         }
         */
-        //Individual i = new Individual();
-        //System.out.println(i.toString());
+
+        Individual i = new Individual();
+        Individual j = new Individual();
+        System.out.println(i.fitness);
+        System.out.println(j.fitness);
+        System.out.println(i.compareTo(i));
+        /*
+        Individual i = new Individual();
+        System.out.println(i.toString());
+        */
     }
 }
